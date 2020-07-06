e89776c8582c532f9fbf98cabb8c53d5df727fda
A18014 <a18014@sangi.jp>
2020-07-06 14:49:31 +0900

    unityちゃんを入れる ＆ ブランチのテスト

---
+++ Assets/Game/Scenes/GameMain/Logic/Script/PlayerCameraAxis.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraAxis : MonoBehaviour
+    {
+    // Start is called before the first frame update
+    private Vector3 offset;      //距離取得用
+    public GameObject Player;
+    void Start()
+        {
+        // MainCamera(自分自身)とplayerとの相対距離を求める
+        offset = transform.position - Player.transform.position;
+
+        }
+
+    // Update is called once per frame
+    void Update()
+        {
+
+        //新しいトランスフォームの値を代入する
+        transform.position = Player.transform.position + offset;
+
+        }
+    }
